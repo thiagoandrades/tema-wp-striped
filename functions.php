6f9b5a918b4dc085f287c2e4511d06893250f994
thiagoandrades <dev@thiagoandrade.me>
2022-04-07 21:50:30 -0300

    adicionando informaçoes do main.css no functions.php

--- functions.php
+++ functions.php
@@ -2,11 +2,13 @@
 
 	function striped_wp_styles (){
 
-		wp_enqueue_style ('style_css', get_stylesheet() );
-		wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/assets/css/main.css');
+		wp_enqueue_style('style_css', get_stylesheet_directory_uri() );
+		wp_enqueue_style('main_css', get_stylesheet_directory_uri () . '/assets/css/main.css');
+		wp_enqueue_style('fontawesome', get_stylesheet_directory_uri () . '/assets/css/fontawesome-all.min.css');
+		wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700|Open+Sans+Condensed:300,700');
 
-		add_action('wp_enqueue_scripts', 'striped_wp_styles');
 	}
+	add_action('wp_enqueue_scripts', 'striped_wp_styles');
 
 
  ?>
