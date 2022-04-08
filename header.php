8d53b8f158d22173642c0c96b871b0a7de1c91c3
thiagoandrades <dev@thiagoandrade.me>
2022-04-07 22:18:21 -0300

    adicionando linguagem e charset dinâmico

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <head>
-		<meta charset="utf-8" />
+	 	<meta charset="<?php bloginfo('charset'); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<?php wp_head(); ?>
 </head>
